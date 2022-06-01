722e5324240844c6ba93fca775ff7ac8b6e9ab20
benaskubilius <kubiliusb2003@gmail.com>
2022-06-01 17:19:58 +0300

    Adding empty IB

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,46 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var Result: UILabel!
+    
+    @IBAction func Clear(_ sender: Any) {
+    }
 
+    @IBAction func Negative(_ sender: Any) {
+    }
+    @IBAction func Percent(_ sender: Any) {
+    }
+    @IBAction func Divide(_ sender: Any) {
+    }
+    @IBAction func Times(_ sender: Any) {
+    }
+    @IBAction func Minus(_ sender: Any) {
+    }
+    @IBAction func Plus(_ sender: Any) {
+    }
+    @IBAction func Equal(_ sender: Any) {
+    }
+    @IBAction func Comma(_ sender: Any) {
+    }
+    @IBAction func Zero(_ sender: Any) {
+    }
+    @IBAction func One(_ sender: Any) {
+    }
+    @IBAction func Two(_ sender: Any) {
+    }
+    @IBAction func Three(_ sender: Any) {
+    }
+    @IBAction func Four(_ sender: Any) {
+    }
+    @IBAction func Five(_ sender: Any) {
+    }
+    @IBAction func Six(_ sender: Any) {
+    }
+    @IBAction func Seven(_ sender: Any) {
+    }
+    @IBAction func Eight(_ sender: Any) {
+    }
+    @IBAction func Nine(_ sender: Any) {
+    }
 }
 
